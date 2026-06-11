61947e119cebff603865038e8a8eb26ab153e7e1
robert <largomoralesrobertjackson@gmail.com>
2026-06-11 12:44:42 +0200

    mejoras generales

---
+++ manipulation-h.js
@@ -0,0 +1,6 @@
+const botn = document.getElementById('btnCambiar');
+
+botn.addEventListener('click', () =>{
+    document.getElementById('potter-broom').classList.toggle('oculto');
+    document.getElementById('XD').classList.toggle('oculto');
+});
